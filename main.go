9b2a69ffef46d8b11455e49f07bce6b4b2772d5d
Carlo Bejoc <carlo.bejoc@gmail.com>
2024-04-18 19:11:19 +0800

    added board

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
 	sdkconnmgr "github.com/flarehotspot/sdk/api/connmgr"
 	sdkhttp "github.com/flarehotspot/sdk/api/http"
 	sdkplugin "github.com/flarehotspot/sdk/api/plugin"
@@ -14,45 +10,24 @@ func main() {}
 
 func Init(api sdkplugin.PluginApi) {
 	// define the portal route
-	api.Http().VueRouter().PortalItemsFunc(func(clnt sdkconnmgr.ClientDevice) []sdkhttp.VuePortalItem {
-		portalItem := sdkhttp.VuePortalItem{
-			Label:     "Play TETRIS",
-			RouteName: "portal.tetris",
-			//RouteParams: map[string]string{"name": "CALOY"},
-		}
-		return []sdkhttp.VuePortalItem{portalItem}
-	})
-
-	// define the portal route
 
 	portalRoute := sdkhttp.VuePortalRoute{
 		RouteName: "portal.tetris",
 		RoutePath: "/tetris",
 		Component: "portal/tetris.vue",
-		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
-			// params := api.Http().MuxVars(r)
-			name := "CALOY"
-
-			data := map[string]interface{}{
-				"name": name,
-			}
-
-			api.Http().VueResponse().Json(w, data, 200)
-		},
-		Middlewares: []func(http.Handler) http.Handler{},
 	}
+
 	// register portal route
 	api.Http().VueRouter().RegisterPortalRoutes(portalRoute)
 
-	api.Http().HttpRouter().PluginRouter().Post("/payments/receieved", func(w http.ResponseWriter, r *http.Request) {
-		var data struct {
-			Amount int `json:"amount"`
+	api.Http().VueRouter().PortalItemsFunc(func(clnt sdkconnmgr.ClientDevice) []sdkhttp.VuePortalItem {
+		portalItem := sdkhttp.VuePortalItem{
+			IconPath:  "icons/tetris.png",
+			Label:     "Play TETRIS",
+			RouteName: "portal.tetris",
+			// RouteParams: map[string]string{},
 		}
-		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(w, "Payment Received: %d", data.Amount)
+		return []sdkhttp.VuePortalItem{portalItem}
+	})
 
-	}).Name("payment.received")
 }
--- resources/components/portal/tetris.vue
+++ resources/components/portal/tetris.vue
@@ -1,36 +1,102 @@
-
-
-
 <template>
-    <form @click.prevent="Submit">
-      <label>Amount:</label>
-      <input type="text" v-model="amount">
-      <button type="submit">Pay</button>
-    </form>
-  </template>
-  
-  <script>
-  define(function(){
+    <div id="background" >
+       Score : <div id="score">0</div> 
+      <canvas id="gameborder"  width="300" height="600"> </canvas> 
+      <!-- <canvas id="myCanvas" width="300" height="150" style="border:1px solid grey"></canvas> -->
+
+    </div>
+    
+</template>
+
+
+
+
+
+
+<script>
+
+    define(function () {
+
     return {
-      props: ['flareView'],
-      template: template,
-      data: function() {
-        return {amount: 0};
-      },
-      methods: {
-        submit: function() {
-          var self = this;
-          var formData = {amount: self.amount};
+        template: template,
+        mounted: function(){
+
+          const canvas = document.getElementById("gameborder");
+          const ctx = canvas.getContext("2d");
+         
+          const SQ = 30;
+          const ROW = 20;
+          const COLUMN = 10;
+          const VACANT = "white";
   
-          $flare.http.post('<% .Helpers.UrlForRoute "payment.received" %>', formData)
-            .then(function(response){
-              console.log(response);
-            })
-            .catch(function(error){
-              console.log(error);
-            });
+
+
+          function drawSquare(x,y,color){
+            ctx.fillStyle = color;
+            ctx.fillRect(x*SQ, y*SQ, SQ, SQ);
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(x*SQ, y*SQ, SQ, SQ);
+          }
+
+
+          // let board = [];
+
+          // for (r=0; r<ROW ; r++){
+          //   board[r] = [];
+          //   for(c =0; c < COLUMN; c++){
+          //       board[r][c] = VACANT;
+          //   }
+          // }
+
+          // function drawBoard(){
+          //   for(r=0; r<ROW ; r++){
+          //     for (r=0; r<COLUMN; r++){
+          //       drawSquare(c,r,board[r][c]);
+          //     }
+          //   }
+          // }
+          
+          // drawBoard()
+
+ 
         }
-      }
-    };
-  });
-  </script>
+     }
+    
+    
+    })
+
+</script>
+
+
+
+
+
+
+<style>
+
+ #gameborder{
+
+    border: 1px  solid black;
+    
+   
+ }
+
+
+
+ #score{
+            display: inline-block;
+        }
+        div{
+            font-size: 25px;
+            font-weight: bold;
+            font-family: monospace;
+            text-align: center;
+        }
+        canvas{
+            display: block;
+            margin: 0 auto;
+            
+        }
+
+
+</style>
